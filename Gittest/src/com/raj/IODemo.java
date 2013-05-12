dbcdbdae721efaf652af5bc7db22bb3b8832cace
nagarajuatgmail <raju.veeranala@gmail.com>
2013-05-12 17:44:47 +0530

    Added file

---
+++ Gittest/src/com/raj/IODemo.java
@@ -0,0 +1,47 @@
+package com.raj;
+
+import java.io.*;
+
+class IODemo
+{
+	public static void main(String[] args) 
+	{
+	  //use ctrl z for termination.in case of characters ctrl-z returns -1;
+	  //in case of strings is return null
+ 	  int i=0;short s;
+      //first method
+      byte barr[]=new byte[5];
+	  System.out.println("Enter five digit integer:");
+ 	  BufferedInputStream in1=new BufferedInputStream(System.in);
+      try{ 	 
+ 	  in1.read(barr);
+	       }catch(IOException ioe){} 
+      System.out.println("string value="+new String(barr));
+	  i=new Integer(new String(barr)).intValue();
+	  System.out.println("integer value="+i);
+      
+     
+      //second method 
+  	  //char temp='\u0000';
+      int temp=0;
+	  String str="";
+  	  System.out.println("Enter any string:");
+	  BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+	  do
+	   {
+	    //read() returns an int
+		//readLine();
+     	try{
+		temp=in.read();
+		   }catch(IOException ioe){}
+		str=str+(char)temp;
+		}while(temp!=-1);
+      System.out.println(str);
+
+      System.out.println("Enter any string:");
+      try{
+	  System.out.println(in.readLine());	
+	     }catch(IOException ioe){}
+
+	}
+};
