9f8cc148f4370cc10f51ce0b1f93348756b3c9b1
nagarajuvatgmail <raju.veeranala@gmail.com>
2013-05-13 15:16:35 +0530

    To Test Proxy

--- Gittest/src/com/raj/GitHello.java
+++ Gittest/src/com/raj/GitHello.java
@@ -9,6 +9,7 @@ public class GitHello {
 		System.out.println("Hello Git");
 		System.out.println("Suneel Added this");
 		System.out.println("Added By Raju");
+		System.out.println("For Proxy Test");
 	}
 
 }
